65355ed1d3f9c6bbf86649ea7750bcc06101782c
Jose <joseebocci@gmail.com>
2020-11-03 19:58:21 -0300

    Commit Sin Enemigos

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	public float parallaxSpeed = 0.5f;
+	public RawImage background;
+	public GameObject uiIdle;
+	public GameObject greenFire;
+
+	public enum GameState {Idle, Playing}
+	public GameState gameState = GameState.Idle;
+
+	public GameObject player;
+
+	void Start()
+	{
+		greenFire.SetActive(false);
+	}
+
+	void Update()
+	{
+		if (gameState == GameState.Idle && Input.GetKeyDown("up"))
+		{
+			gameState = GameState.Playing;
+			greenFire.SetActive(true);
+			uiIdle.SetActive(false);
+			player.SendMessage("UpdateState", "CorriendoMedio");
+			player.SendMessage("UpdatePosicion", 1);
+		}
+		else if (gameState == GameState.Playing)
+		{
+			Parallax();
+		}
+	}
+
+	void Parallax()
+    {
+		float finalSpeed = parallaxSpeed * Time.deltaTime;
+		background.uvRect = new Rect(background.uvRect.x + finalSpeed, 0f, 1f, 1f);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator animator;
+    int posicion = 1;
+    public void UpdatePosicion(int posicion)
+    {
+        this.posicion = posicion;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (posicion == 1)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+                UpdateState("CarrilArriba");
+                UpdatePosicion(2);
+            }else if (Input.GetKeyDown("down"))
+            {
+                UpdateState("CarrilAbajo");
+                UpdatePosicion(0);
+            }
+        }
+        if (posicion == 2)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+                UpdateState("CarrilMedioArriba");
+                UpdatePosicion(1);
+            }
+        }
+        if (posicion == 0)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+                UpdateState("CarrilMedioAbajo");
+                UpdatePosicion(1);
+            }
+            else if (Input.GetKeyDown("down"))
+            {
+            }
+        }
+    }
+
+ 
+
+    public void UpdateState(string state = null)
+    {
+        if (state != null)
+        {
+            animator.Play(state);
+        }
+    }
+
+}
